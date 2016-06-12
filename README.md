886b535a1430d28acc542fcfe67a3bda7c37e939
Peter Briggeman <pbriggeman@gmail.com>
2016-06-11 22:22:38 -0400

    Configuring application to run Pug instead of Jade

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Simple Node/Express AJAX File Upload
+
+After scouring the interwebs for an example of Node/Express example of just a very simple single file upload through AJAX and not finding anything, I decided to create my own example using a combination of the examples that I did find.  There are times where users just need a simple single file upload to go along with some content on a form and you may not want the form to refresh when you upload the image to the server. 
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var randomstring = require("randomstring");
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -12,7 +13,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -27,12 +28,32 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
+
+app.post('/upload', function(req, res) {
+	var sampleFile;
+
+	if (!req.files) {
+		res.send('No files were uploaded.');
+		return;
+	}
+
+	sampleFile = req.files.upload;
+    var filename = randomstring.generate(12) + "_" + sampleFile.name;
+	sampleFile.mv('/home/pbriggeman/Projects/sales_ios_apps/assets/' + filename, function(err) {
+		if (err) {
+			res.status(500).send(err);
+		}
+		else {
+			res.send({'err':0,'filename':filename});
+		}
+	});
+});
 
 // development error handler
 // will print stacktrace
